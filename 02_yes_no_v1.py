71765719a04286ea0e2b16484ece5a09205610a3
David Susanibar <99370876+David1Susanibar@users.noreply.github.com>
2022-02-24 00:13:32 +0000

    added exit code to yes / no

--- 02_yes_no_v1.py
+++ 02_yes_no_v1.py
@@ -1,12 +1,19 @@
-show_instructions = input("have you played this game before?" ).lower()
-if show_instructions == "yes":
-  print("program continues")
-elif show_instructions == "y":
-  print("Program continues")
+show_instructions = ""
+while show_instructions != "xxx":
 
-elif show_instructions == "no":
-  print("Display instructions")
-elif show_instructions == "n":
-  print("Display instructions")
-else:
-  print("Please answer yes / no")
+
+  show_instructions = input("have you played this game before?" ).lower()
+
+  if show_instructions == "xxx":
+    break
+  elif show_instructions == "yes":
+    print("program continues")
+  elif show_instructions == "y":
+    print("Program continues")
+  
+  elif show_instructions == "no":
+    print("Display instructions")
+  elif show_instructions == "n":
+    print("Display instructions")
+  else:
+    print("Please answer yes / no")
